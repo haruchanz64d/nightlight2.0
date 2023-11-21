08b72a6c5733b466b93888885b59e1e99f5e37be
Hitori <52887342+hitoriiichan@users.noreply.github.com>
2023-11-21 08:39:49 +0800

    Added the new Input System

---
+++ Assets/Scripts/Game Systems/Input System/InputManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+    
+    public float MovementInput { get; private set; }
+    public bool JumpPressed { get; private set; }
+    public bool JumpHeld { get; private set; }
+    public bool JumpReleased { get; private set; }
+    public bool DashInput { get; private set; }
+    public bool PauseInput { get; private set; }
+    public bool InteractInput { get; private set; }
+
+    private PlayerInput playerInput;
+
+    private InputAction movementAction;
+    private InputAction jumpAction;
+    private InputAction dashAction;
+    private InputAction pauseAction;
+    private InputAction interactAction;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        playerInput = GetComponent<PlayerInput>();
+        SetupInputActions();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateInput();
+    }
+
+    private void SetupInputActions()
+    {
+        movementAction = playerInput.actions["Movement"];
+        jumpAction = playerInput.actions["Jump"];
+        dashAction = playerInput.actions["Dash"];
+        pauseAction = playerInput.actions["Pause"];
+        interactAction = playerInput.actions["Interact"];
+    }
+    private void UpdateInput()
+    {
+        MovementInput = movementAction.ReadValue<float>();
+        JumpPressed = jumpAction.WasPressedThisFrame();
+        JumpHeld = jumpAction.WasPerformedThisFrame();
+        JumpReleased = jumpAction.WasReleasedThisFrame();
+        DashInput = dashAction.WasPressedThisFrame();
+        PauseInput = pauseAction.WasPressedThisFrame();
+        InteractInput = interactAction.WasPressedThisFrame();
+    }
+}
