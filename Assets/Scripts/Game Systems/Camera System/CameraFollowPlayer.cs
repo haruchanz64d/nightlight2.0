3dc4f6d504d3615ac234498826f0287d091548bc
Hitori <52887342+hitoriiichan@users.noreply.github.com>
2023-11-21 12:38:00 +0800

    Added CameraSystem

---
+++ Assets/Scripts/Game Systems/Camera System/CameraFollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float flipYRotationTime = 0.5f;
+
+    private Coroutine turnCoroutine;
+    private Player player;
+    private bool isFacingRight;
+
+    private void Awake()
+    {
+
+    }
+}
