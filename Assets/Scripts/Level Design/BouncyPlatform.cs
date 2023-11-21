f710812392eb68696006d4fd5d686fda72c100c8
Hitori <radam.aronjake@gmail.com>
2023-11-21 10:40:04 +0800

    Created BouncyPlatform

---
+++ Assets/Scripts/Level Design/BouncyPlatform.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace LunarflyArts
+{
+    public class BouncyPlatform : MonoBehaviour
+    {
+        private float bounceForce = 20f;
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
