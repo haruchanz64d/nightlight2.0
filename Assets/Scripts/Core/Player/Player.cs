2243449469dd56940c2acfa9cf5f15917dc222f3
Hitori <52887342+hitoriiichan@users.noreply.github.com>
2023-12-04 04:26:03 +0100

    Update

--- Assets/Scripts/Core/Player/Player.cs
+++ Assets/Scripts/Core/Player/Player.cs
@@ -1,59 +1,38 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 using System.Collections;
 public class Player : MonoBehaviour
 {
     [Header("Movement")]
     private float moveInput;
     [SerializeField] private float movementSpeed = 7.5f;
-    public bool isFacingRight;
+    public bool isFacingRight = true;
     [Header("Jump")]
     [SerializeField] private float jumpForce = 12f;
     private float jumpTimeCounter;
     [SerializeField] private float jumpTime = 0.35f;
     private bool isJumping;
-    private bool isFalling;
     [SerializeField] private float maxFallSpeed = 12f;
     [Header("Ground Check")]
     [SerializeField] private float extraHeight = 0.25f;
     [SerializeField] private LayerMask layerMask;
-    [Header("Camera Stuff")]
-    [SerializeField] private GameObject cameraObject;
-    private float fallSpeedYDampingChangeThreshhold;
-    
+
     private Rigidbody2D rb;
     private Animator animator;
     private new Collider2D collider2D;
     private RaycastHit2D raycastHit2D;
-    private CameraFollow cameraFollow;
-   
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         collider2D = GetComponent<Collider2D>();
         StartDirectionCheck();
-
-        cameraFollow = cameraObject.GetComponent<CameraFollow>();
-
-        fallSpeedYDampingChangeThreshhold = CameraManager.instance.fallSpeedYDampingThreshhold;
     }
 
     private void Update()
     {
         Move();
         Jump();
-
-        if (rb.velocity.y < fallSpeedYDampingChangeThreshhold && !CameraManager.instance.IsLerpingYDamping && !CameraManager.instance.LerpedFromFalling)
-        {
-            CameraManager.instance.LerpYDamping(true);
-        }
-        
-        if (rb.velocity.y > fallSpeedYDampingChangeThreshhold && !CameraManager.instance.IsLerpingYDamping && CameraManager.instance.LerpedFromFalling)
-        {
-            CameraManager.instance.LerpedFromFalling = false;
-
-            CameraManager.instance.LerpYDamping(false);
-        }
     }
 
     private void FixedUpdate()
@@ -74,7 +53,6 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        // Se il pulsante è stato premuto in questo fotogramma.
         if (UserInput.instance.inputActions.Gameplay.Jump.WasPressedThisFrame() && IsGrounded())
         {
             isJumping = true;
@@ -82,17 +60,15 @@ public class Player : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
-        // Se il pulsante è stato tenuto premuto.
         if (UserInput.instance.inputActions.Gameplay.Jump.IsPressed())
         {
-            if(jumpTimeCounter > 0 && isJumping)
+            if (jumpTimeCounter > 0 && isJumping)
             {
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
                 jumpTimeCounter -= Time.deltaTime;
             }
-            else if(jumpTimeCounter == 0)
+            else if (jumpTimeCounter == 0)
             {
-                isFalling = true;
                 isJumping = false;
             }
             else
@@ -101,52 +77,25 @@ public class Player : MonoBehaviour
             }
         }
 
-        // Se il pulsante è stato rilasciato in questo fotogramma.
         if (UserInput.instance.inputActions.Gameplay.Jump.WasReleasedThisFrame())
         {
             isJumping = false;
-            isFalling = true;
-        }
-
-        if(!isJumping && CheckForLand())
-        {
-            // ???
         }
     }
     #endregion
 
-    #region Ground / Land Check
-    private bool IsGrounded() {
+    #region Ground Check
+    private bool IsGrounded()
+    {
         raycastHit2D = Physics2D.BoxCast(collider2D.bounds.center, collider2D.bounds.size, 0f, Vector2.down, extraHeight, layerMask);
         if (raycastHit2D.collider != null) return true;
         else return false;
-    }
-
-    private bool CheckForLand()
-    {
-        if(isFalling)
-        {
-            if(IsGrounded())
-            {
-                isFalling = false;
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return false;
-        }
     }
     #endregion
     #region Turn Checks
     private void StartDirectionCheck()
     {
-        if(transform.position.x > 0f) isFacingRight = true;
+        if (transform.position.x > 0f) isFacingRight = true;
         else isFacingRight = false;
     }
 
@@ -158,21 +107,17 @@ public class Player : MonoBehaviour
 
     private void Turn()
     {
-        if(isFacingRight)
+        if (isFacingRight)
         {
             Vector3 rotate = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
             transform.rotation = Quaternion.Euler(rotate);
             isFacingRight = !isFacingRight;
-
-            cameraFollow.CallTurn();
         }
         else
         {
             Vector3 rotate = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
             transform.rotation = Quaternion.Euler(rotate);
             isFacingRight = !isFacingRight;
-
-            cameraFollow.CallTurn();
         }
     }
     #endregion
