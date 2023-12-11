bc6761ff2db78575d9d8ec52a96525b135db1edf
Hitori <radam.aronjake@gmail.com>
2023-12-11 18:33:30 +0800

    Update

--- Assets/Scripts/ClearPlayerData.cs
+++ Assets/Scripts/ClearPlayerData.cs
@@ -1,21 +1,26 @@
+using System;
 using System.IO;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 public class ClearPlayerData : MonoBehaviour
 {
     public static void ClearPlayerPrefs()
     {
         PlayerPrefs.DeleteAll();
-        Debug.Log("All PlayerPref keys removed!");
     }
 
     public static void ClearPlayerDataJson()
     {
         string filePath = Path.Combine(Application.persistentDataPath, "playerStats.json");
-        if (File.Exists(filePath))
+        try
         {
-            File.Delete(filePath);
-            Debug.Log("Player Data had been removed!");
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error deleting file: " + e.Message);
         }
     }
 
@@ -23,5 +28,7 @@ public class ClearPlayerData : MonoBehaviour
     {
         ClearPlayerPrefs();
         ClearPlayerDataJson();
+
+        Debug.Log("Player Prefs & PlayerStats.json had been removed!");
     }
 }
